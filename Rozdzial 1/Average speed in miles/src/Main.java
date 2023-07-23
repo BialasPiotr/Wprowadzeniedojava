1b6db50f4e7b7328ab9f4d58332e087d91f1e2fe
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 18:16:02 +0200

    Task 1.10

---
+++ Rozdzial 1/Average speed in miles/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double Mila = 1.6;
+        int KM = 14;
+        double srednia = KM / Mila  ;
+        System.out.println("Srednia predkosc w milach wynosi " + srednia + " na godzine");
+    }
+}
