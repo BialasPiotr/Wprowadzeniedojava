e8f963690ffef4c849c171d36df100e5cd7043e0
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 18:11:13 +0200

    Task 1.8

---
+++ Rozdzial 1/Area and circumference of a circle/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double PI = 3.14;
+        double promien = 5.5;
+        double obwod = 2 * promien * PI;
+        double powierzchnia = promien * promien * PI;
+        System.out.println("Obwod koła o promieniu 5.5 wynosi:  " + obwod);
+        System.out.println("Powierzchnia kola wynosi: " + powierzchnia);
+    }
+}
