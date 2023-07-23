b136124f167c5b47e912687f67da684364249a53
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 14:18:03 +0200

    Chapter 1 and first two tasks

---
+++ Rozdzial 1/Display five messages/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Witaj w javie");
+        System.out.println("Witaj w javie");
+        System.out.println("Witaj w javie");
+        System.out.println("Witaj w javie");
+        System.out.println("Witaj w javie");
+    }
+}
---
+++ Rozdzial 1/Display three messages/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Witaj w Javie, Witaj informatyki i Programowanie to przyjemnosc!");
+    }
+}
