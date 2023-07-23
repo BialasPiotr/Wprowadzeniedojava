2a55520595f038e411d2e9b6c9f78f3d11ac7871
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 18:13:15 +0200

    Task 1.9

---
+++ Rozdzial 1/Area and perimeter of a rectangle/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double wysokosc = 7.9;
+        double szerokosc = 4.5;
+        double powierzchnia = szerokosc * wysokosc;
+        System.out.println(" Powierzchnia prostokatna wynosi: " + powierzchnia);
+    }
+}
