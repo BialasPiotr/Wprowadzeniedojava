29586be50f6cc5e46c02c97ac30c6e6199ae60b7
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 14:24:10 +0200

    Task 1.5

---
+++ Rozdzial 1/Calculating expressions/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double x;
+        double y;
+        x = 9.5 * 4.5 - 2.5 * 3;
+        y = 45.5 - 3.5;
+
+        System.out.println(x/y);
+    }
+}
