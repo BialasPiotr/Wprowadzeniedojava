8939f10e50854b887fa2c4c69208f31c97394d1d
Piotr Białas <96840701+BialasPiotr@users.noreply.github.com>
2023-07-23 14:19:32 +0200

    task 1.3

---
+++ Rozdzial 1/Pattern display/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("   J   A  V       V  A");
+        System.out.println("   J   AA  V     V   AA");
+        System.out.println("J  J  AAAA  V   V   AAAA");
+        System.out.println(" JJ  A    A   V    A    A");
+    }
+}
